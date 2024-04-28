6136d5e0ceae2c1206eed8764d4968ae82a92661
FrightBoy <157490452+FrightBoy@users.noreply.github.com>
2024-04-28 19:36:15 +0500

    Update_2.1
    Enabling Api.routers for todo's

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from database import Base, engine
-from API.user_api.user_api import user_router
 from fastapi.staticfiles import StaticFiles
+
+# APIs import
+from API.user_api.user_api import user_router
+from API.todo_api.todo_api import todo_router
 
 # команда для создания всех таблиц в дб
 Base.metadata.create_all(bind=engine)
@@ -9,3 +12,4 @@ app = FastAPI(docs_url="/")
 app.mount("/static", StaticFiles(directory="static"))
 
 app.include_router(user_router)
+app.include_router(todo_router)
